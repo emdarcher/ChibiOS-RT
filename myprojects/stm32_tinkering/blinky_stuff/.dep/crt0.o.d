f025586de2222e556aec00b1ef4a763f8e2ef044
Ethan Durrant <emd519@yahoo.com>
2014-09-26 13:35:53 -0700

    made working code using hsi i think

---
+++ myprojects/stm32_tinkering/blinky_stuff/.dep/crt0.o.d
@@ -0,0 +1,13 @@
+build/obj/crt0.o: ../../../os/common/ports/ARMCMx/compilers/GCC/crt0.c \
+ /usr/lib/gcc/arm-none-eabi/4.8.4/include/stdint.h \
+ /usr/arm-none-eabi/include/stdint.h \
+ /usr/arm-none-eabi/include/machine/_default_types.h \
+ /usr/arm-none-eabi/include/sys/features.h
+
+/usr/lib/gcc/arm-none-eabi/4.8.4/include/stdint.h:
+
+/usr/arm-none-eabi/include/stdint.h:
+
+/usr/arm-none-eabi/include/machine/_default_types.h:
+
+/usr/arm-none-eabi/include/sys/features.h:
---
+++ myprojects/stm32_tinkering/blinky_stuff/.dep/vectors.o.d
@@ -0,0 +1,38 @@
+build/obj/vectors.o: \
+ ../../../os/common/ports/ARMCMx/compilers/GCC/vectors.c \
+ /usr/lib/gcc/arm-none-eabi/4.8.4/include/stdbool.h \
+ /usr/lib/gcc/arm-none-eabi/4.8.4/include/stdint.h \
+ /usr/arm-none-eabi/include/stdint.h \
+ /usr/arm-none-eabi/include/machine/_default_types.h \
+ /usr/arm-none-eabi/include/sys/features.h \
+ ../../../os/common/ports/ARMCMx/devices/STM32F1xx/cmparams.h \
+ ../../../os/hal/boards/OLIMEX_STM32_P103/board.h \
+ ../../../os/ext/CMSIS/ST/stm32f10x.h \
+ ../../../os/ext/CMSIS/include/core_cm3.h \
+ ../../../os/ext/CMSIS/include/core_cmInstr.h \
+ ../../../os/ext/CMSIS/include/core_cmFunc.h \
+ ../../../os/ext/CMSIS/ST/system_stm32f10x.h
+
+/usr/lib/gcc/arm-none-eabi/4.8.4/include/stdbool.h:
+
+/usr/lib/gcc/arm-none-eabi/4.8.4/include/stdint.h:
+
+/usr/arm-none-eabi/include/stdint.h:
+
+/usr/arm-none-eabi/include/machine/_default_types.h:
+
+/usr/arm-none-eabi/include/sys/features.h:
+
+../../../os/common/ports/ARMCMx/devices/STM32F1xx/cmparams.h:
+
+../../../os/hal/boards/OLIMEX_STM32_P103/board.h:
+
+../../../os/ext/CMSIS/ST/stm32f10x.h:
+
+../../../os/ext/CMSIS/include/core_cm3.h:
+
+../../../os/ext/CMSIS/include/core_cmInstr.h:
+
+../../../os/ext/CMSIS/include/core_cmFunc.h:
+
+../../../os/ext/CMSIS/ST/system_stm32f10x.h:
